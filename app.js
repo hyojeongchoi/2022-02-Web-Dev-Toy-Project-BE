5b3e3a0d46b56e407d9804f9d8c33bf7f5c51b4a
Queue-ri <qriositylog@gmail.com>
2023-01-12 21:08:11 +0900

    feat: Add GCP file uploader

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const redis = require('redis');
 const jwt = require('jsonwebtoken');
+const Multer = require('multer');
+
+// env
+const dotenv = require("dotenv");
+dotenv.config();
+
+// GCP Storage
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+const bucket = storage.bucket(process.env.GCP_STORAGE_BUCKET); // dotenv 필요
+
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 app.use(express.json());
 
@@ -11,6 +24,42 @@ const prisma = new PrismaClient()
 
 const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(process.env.BE_CLIENT_ID, process.env.BE_CLIENT_SECRET);
+
+// Multer
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024, // no larger than 10mb
+  },
+});
+
+// GCP image upload test
+app.post('/upload', multer.single('file'), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send('No file uploaded.');
+    return;
+  }
+  console.log(req.file)
+
+  const fileExtension = req.file.originalname.split('.').pop();
+  const fileName = uuidv4(); // uuid4
+  const filePath = `posts/test/${fileName}.${fileExtension}`;
+  const blob = bucket.file(filePath);
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+  });
+
+  blobStream.on('error', err => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+    res.status(200).send(`https://storage.googleapis.com/${bucket.name}/` + filePath);
+  });
+
+  blobStream.end(req.file.buffer);
+});
+
 
 //////////////////////////////// REDIS //////////////////////////////////
 
@@ -240,9 +289,34 @@ app.post('/token', authenticateAccessToken, (req, res) => {
   res.send({ accessToken: accessToken });
 })
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+
+///////////////////////////// Image Upload //////////////////////////////
+
+// 이미지 업로드 & URL 반환하는 함수
+function uploadImage(req, res, next) {
+  // file이 존재해야만 업로드
+  if(req.file != undefined){
+    const fileExtension = req.file.originalname.split('.').pop();
+    const fileName = uuidv4(); // uuid4
+    const filePath = `posts/test/${fileName}.${fileExtension}`;
+    const blob = bucket.file(filePath);
+    const blobStream = blob.createWriteStream({
+        resumable: false,
+    });
+  
+    blobStream.on('error', err => {
+        next(err);
+    });
+  
+    blobStream.on('finish', () => {
+        const image = `https://storage.googleapis.com/${bucket.name}/` + filePath;
+        req.image = image;
+        next();
+    });
+  
+    blobStream.end(req.file.buffer);
+  }
+};
 
 // GCP Storage 파일 삭제하는 함수 -> 삭제 딜레이 2-30분 있는지 확인하기
 async function deleteFile(image) {
