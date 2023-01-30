4141676ec0925b6045c35fa1a1a4e7262fdc96e5
Skyminn <rucy0716@naver.com>
2023-01-30 18:34:08 +0900

    fix: Modify migration.sql

--- prisma/migrations/20230130093141_create_categories/migration.sql
+++ prisma/migrations/20230130093141_create_categories/migration.sql
@@ -27,6 +27,7 @@ CREATE TABLE `Posts` (
     `place` VARCHAR(100) NOT NULL,
     `status` VARCHAR(20) NOT NULL,
     `tag` VARCHAR(100) NOT NULL,
+    `postStatus` VARCHAR(20) NOT NULL,
 
     UNIQUE INDEX `Posts_postId_key`(`postId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -56,6 +57,29 @@ CREATE TABLE `Alarm` (
     PRIMARY KEY (`alarmId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `comment_alarm` (
+    `cAlarmId` BIGINT NOT NULL AUTO_INCREMENT,
+    `userId` BIGINT NOT NULL,
+    `commentId` BIGINT NOT NULL,
+    `postId` BIGINT NOT NULL,
+    `uniqueId` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(500) NOT NULL,
+    `readStatus` VARCHAR(20) NOT NULL,
+
+    UNIQUE INDEX `comment_alarm_uniqueId_key`(`uniqueId`),
+    PRIMARY KEY (`cAlarmId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `comment_alarmoff` (
+    `cAlarmOffId` BIGINT NOT NULL AUTO_INCREMENT,
+    `userId` BIGINT NOT NULL,
+    `postId` BIGINT NOT NULL,
+
+    PRIMARY KEY (`cAlarmOffId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Comment` ADD CONSTRAINT `Comment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Users`(`userId`) ON DELETE SET NULL ON UPDATE CASCADE;
 
@@ -67,3 +91,18 @@ ALTER TABLE `Alarm` ADD CONSTRAINT `Alarm_userId_fkey` FOREIGN KEY (`userId`) RE
 
 -- AddForeignKey
 ALTER TABLE `Alarm` ADD CONSTRAINT `Alarm_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Posts`(`postId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment_alarm` ADD CONSTRAINT `comment_alarm_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Users`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment_alarm` ADD CONSTRAINT `comment_alarm_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `Comment`(`commentId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment_alarm` ADD CONSTRAINT `comment_alarm_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Posts`(`postId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment_alarmoff` ADD CONSTRAINT `comment_alarmoff_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `Users`(`userId`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment_alarmoff` ADD CONSTRAINT `comment_alarmoff_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Posts`(`postId`) ON DELETE RESTRICT ON UPDATE CASCADE;
