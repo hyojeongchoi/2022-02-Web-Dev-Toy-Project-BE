c0bb255e4679aeed8dadc028dee791c46289d16b
chaeeun-Han <chaeeun.h911@gmail.com>
2023-02-04 23:04:28 +0900

    fix: Modify endpoints

--- routes/Comment.js
+++ routes/Comment.js
@@ -8,26 +8,26 @@ const { authenticateAccessToken } = require('../middlewares/Auth');
 
 
 // 댓글 목록 조회 api (시간 순으로 오름차순 정렬하기)
-router.get('/', async (req, res) => {
+router.get('/:postId', async (req, res) => {
   try {
-    const postId = Number(req.params.id);
-    
+    const postId = Number(req.params.postId);
+
     const commentList = await prisma.comment.findMany({
-        orderBy: [
-          {
-            // commentDate 값이 일치하는 데이터들이 있을 수 있음. -> unique한 값을 갖고 생성순으로 ID값이 증가하는 PK로 정렬
-            commentId: 'asc'
-          }
-        ],
-        where: {
-          // postId가 일치하면 데이터 조회
-          postId: postId
-        },
-        select: {
-          userId: true,
-          content: true,
-          commentDate: true
+      orderBy: [
+        {
+          // commentDate 값이 일치하는 데이터들이 있을 수 있음. -> unique한 값을 갖고 생성순으로 ID값이 증가하는 PK로 정렬
+          commentId: 'asc'
         }
+      ],
+      where: {
+        // postId가 일치하면 데이터 조회
+        postId: postId
+      },
+      select: {
+        userId: true,
+        content: true,
+        commentDate: true
+      }
     });
     res.send(commentList);
   }
@@ -39,16 +39,24 @@ router.get('/', async (req, res) => {
 
 
 // 댓글 등록 API
-router.post('/', authenticateAccessToken, async (req, res) => {
+router.post('/:postId', authenticateAccessToken, async (req, res) => {
   try {
-    const postId = Number(req.params.id);
-    const userId = req.user.id;
+    const postId = Number(req.params.postId);
+    const userId = Number(req.user.id);
     const content = req.body.content;
 
     await prisma.comment.create({
       data: {
-        postId: postId,
-        userId: userId,
+        post: {
+          connect: {
+            postId: postId,
+          }
+        },
+        user: {
+          connect: {
+            userId: userId,
+          }
+        },
         content: content
       }
     })
@@ -61,10 +69,10 @@ router.post('/', authenticateAccessToken, async (req, res) => {
 
 
 // 댓글 수정 API
-router.put('/:commentId', authenticateAccessToken, async (req, res) => {
+router.put('/:postId/:commentId', authenticateAccessToken, async (req, res) => {
   try {
     const commentId = Number(req.params.commentId);
-    const postId = Number(req.params.id);
+    const postId = Number(req.params.postId);
     const userId = req.user.id;
     const content = req.body.content;
 
@@ -96,9 +104,9 @@ router.put('/:commentId', authenticateAccessToken, async (req, res) => {
 
 
 // 댓글 삭제 API
-router.delete('/:commentId', authenticateAccessToken, async (req, res) => {
+router.delete('/:postId/:commentId', authenticateAccessToken, async (req, res) => {
   try {
-    const postId = Number(req.params.id);
+    const postId = Number(req.params.postId);
     const commentId = Number(req.params.commentId);
     const userId = req.user.id;
 
